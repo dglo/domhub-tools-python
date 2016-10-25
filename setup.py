ce6bc9a8aa5c23a4aa6c3e719970620a3bd37ea8
John Kelley <jkelley@icecube.wisc.edu>
2016-10-25 11:00:33 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='domhub-tools-python',
-      version='1.4.2',
+      version='1.4.3',
       description='IceCube DOMHub Monitoring',
       author='John Kelley',
       author_email='jkelley@icecube.wisc.edu',
