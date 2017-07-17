20790d54752bd191d993b4923d1a44c8d35a8dc2
John Kelley <jkelley@icecube.wisc.edu>
2017-07-16 19:30:58 -0500

    Only pack once

--- fabfile.py
+++ fabfile.py
@@ -25,6 +25,7 @@ def installCronjob(label, job):
     bashstr += 'echo \"%s\"; echo; } | crontab -)' % job
     run(bashstr)
 
+@runs_once
 def pack():
     # create a new source distribution as tarball
     local('python setup.py sdist --formats=gztar', capture=False)
