7b40f0951f21f02ef00dc240133cc62fd9d96cee
John Kelley <jkelley@icecube.wisc.edu>
2018-10-19 14:08:55 -0500

    Issue 13: 8300: hubmoni: add cluster to user alert notifications

--- hubmonitools/moniDOMs.py
+++ hubmonitools/moniDOMs.py
@@ -107,15 +107,16 @@ def moniAlerts(config, dor, hubConfig, hub, cluster):
     # Check number of DOR cards
     if (len(dor.cards) != conf["dor"]):
         alert_txt = "%s: unexpected number of DOR cards" % hub
-        alert_desc = "%s: expected %d DOR cards, found %d" % (hub, conf["dor"], len(dor.cards))
+        alert_desc = "%s-%s: expected %d DOR cards, found %d" % \
+            (cluster, hub, conf["dor"], len(dor.cards))
         alert = HubMoniAlert(config, hub, cluster, alert_txt=alert_txt, alert_desc=alert_desc)
         alerts.append(alert)
         
     # Check number of communicating DOMs
     if (len(dor.getCommunicatingDOMs()) != conf["comm"]):
         alert_txt = "%s: unexpected number of DOMs" % hub
-        alert_desc = "%s: expected %d communicating DOMs, found %d" % \
-            (hub, conf["comm"], len(dor.getCommunicatingDOMs()))
+        alert_desc = "%s-%s: expected %d communicating DOMs, found %d" % \
+            (cluster, hub, conf["comm"], len(dor.getCommunicatingDOMs()))
         alert = HubMoniAlert(config, hub, cluster, alert_txt=alert_txt, alert_desc=alert_desc)        
         alerts.append(alert)
 
@@ -127,7 +128,7 @@ def moniAlerts(config, dor, hubConfig, hub, cluster):
         if not moni.pwrcheck.ok and not hubConfig.isWaived(hub, cluster, int(dom.card), int(dom.pair)):
             if not pwrFail:
                 alert_txt = "%s: DOM power check failure" % hub
-                alert_desc = "%s: " % hub
+                alert_desc = "%s-%s: " % (cluster, hub)
                 alert_desc += moni.pwrcheck.text
                 alert = HubMoniAlert(config, hub, cluster, alert_txt=alert_txt, alert_desc=alert_desc)
                 pwrFail = True
